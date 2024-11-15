bcfd1ba1981fde247a38ce1bc6da0ee3fe009cd9
Golden <salvadorayalaxd@gmail.com>
2024-11-15 20:34:42 +0100

    Fix ftests

--- ftest-soa/ftest_cutfreq_soa.sh
+++ ftest-soa/ftest_cutfreq_soa.sh
@@ -11,27 +11,6 @@ if [ ! -d "release-build" ]; then
     cmake . -DCMAKE_BUILD_TYPE=Release -B release-build
 fi
 
-# Función para ejecutar pruebas
-run_test() {
-  local input_file=$1
-  local cutfreq=$2
-  local output_file=$3
-
-  echo "Running cutfreq test for $input_file with cutfreq $cutfreq"
-
-  # Leer el header de la imagen
-  echo "Reading header for $input_file..."
-  infile="$INPUT_DIR/$input_file"
-  outfile="$OUTPUT_DIR/$output_file"
-
-  # Ejecutar el programa con los parámetros necesarios
-  if ! "$IMTOOL_SOA" "$infile" "$outfile" cutfreq "$cutfreq"; then
-    echo "FAIL: Error generating $output_file."
-  else
-    echo "PASS: $output_file generated successfully."
-  fi
-}
-
 # Test case para lake-large
 echo "Running cutfreq test for lake-large.ppm with cutfreq 100000"
 if ! "$IMTOOL_SOA" "$INPUT_DIR/lake-large.ppm" "$OUTPUT_DIR/lake-large100k-SOA.ppm" cutfreq 100000; then
